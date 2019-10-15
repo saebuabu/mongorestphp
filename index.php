7d87b22fd4acdead0e259f26b11bba4aa75340b8
asaebu <a.saebu@kw1c.nl>
2019-10-15 13:56:40 +0200

    andere test

--- index.php
+++ index.php
@@ -8,10 +8,21 @@ $client = new MongoDB\Client(
     'mongodb+srv://'.USRPASS.'@cluster0-2d82g.azure.mongodb.net/sample_mflix?retryWrites=true&w=majority'
 );
 
+
+try {
+    $dbs = $client->listDatabases();
+}
+catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+print_r($dbs);
+
+/*
 $collection = $client->mydb->customers;
 
 $result = $collection->find()->toArray();
 
 print_r($result);
-
+*/
 
