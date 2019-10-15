78afb16179f96d283b86ced6e09af15dc24c1a21
asaebu <a.saebu@kw1c.nl>
2019-10-15 14:42:30 +0200

    directory hoger

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require 'vendor/autoload.php';
 
-require dirname(__FILE__).'/../keys.php';
+require dirname(__FILE__).'/../../keys.php';
 
 $client = new MongoDB\Client(
     'mongodb+srv://'.USRPASS.'@cluster0-2d82g.azure.mongodb.net/sample_mflix?retryWrites=true&w=majority'
