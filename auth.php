42912ccc30aecada5c3eae97e559e0d065775d73
asaebu <a.saebu@kw1c.nl>
2020-01-14 17:35:50 +0100

    omgezet naar $_POST omdat $data leeg bleef?!

--- auth.php
+++ auth.php
@@ -1,21 +1,24 @@
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 require 'classes/FamilyMemberClient.php';
 require 'classes/JsonIO.php';
 
 // get posted data
-$data = json_decode(file_get_contents("php://input"));
+//$data = json_decode(file_get_contents("php://input"));
 
 // connectie naar database, collectie
 $fmember = new FamilyMemberClient('myfamily', 'family');
-$fmember->Auth($data->login, $data->password);
-if ($fmember->Auth($data->login, $data->password))
+
+
+//$fmember->Auth($data->login, $data->password);
+if ($fmember->Auth($_POST['login'], $_POST['password']))
 {
     family\JsonIO::WriteOk();
 } else {
